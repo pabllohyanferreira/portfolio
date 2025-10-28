0b1a4cbaad03e12c00f80aa4c7db31d99e6a0069
1layerzao <pablohyan64@gmail.com>
2025-10-27 23:19:57 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import Hero from './components/Hero';
-import AboutSection from './components/AboutSection';
 import ProjectsSection from './components/ProjectsSection';
-import BackgroundParticles from './components/BackgroundParticles';
 import ProgrammingElements from './components/ProgrammingElements';
 import SmoothScroll from './components/SmoothScroll';
 import './App.css';
@@ -10,11 +8,9 @@ function App() {
   return (
     <div className="App">
       <SmoothScroll />
-      <BackgroundParticles />
       <ProgrammingElements />
       <Hero />
       <ProjectsSection />
-      <AboutSection />
     </div>
   );
 }
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -5,15 +5,15 @@ interface Project {
   id: string;
   title: string;
   description: string;
-  youtubeId: string;
+  vimeoId: string;
   technologies: string[];
   category: string;
 }
 
 const ProjectsSection: React.FC = () => {
-  const openYouTubeFullscreen = (youtubeId: string) => {
-    const youtubeUrl = `https://www.youtube.com/watch?v=${youtubeId}`;
-    window.open(youtubeUrl, '_blank');
+  const openVimeoFullscreen = (vimeoId: string) => {
+    const vimeoUrl = `https://vimeo.com/${vimeoId}`;
+    window.open(vimeoUrl, '_blank');
   };
 
   const projects: Project[] = [
@@ -21,7 +21,7 @@ const ProjectsSection: React.FC = () => {
       id: 'torres-coffee',
       title: 'Torres Coffee',
       description: 'Sistema completo de cardápio digital para cafeteria com interface administrativa. Desenvolvido com HTML, CSS e JavaScript puro, oferece gerenciamento de produtos, categorias e upload de imagens.',
-      youtubeId: 'vgq-nbfPKeU',
+      vimeoId: '1131126826',
       technologies: ['HTML', 'CSS', 'JavaScript', 'LocalStorage'],
       category: 'Web Development'
     },
@@ -29,7 +29,7 @@ const ProjectsSection: React.FC = () => {
       id: 'gueiros',
       title: 'DEPARA Gueiros',
       description: 'Sistema profissional de sincronização automática de arquivos entre empresa e contador, rodando como Serviço do Windows (inicia automaticamente com o sistema).',
-      youtubeId: 'hpAHvcLaji0',
+      vimeoId: '1131126922',
       technologies: ['Windows Service', 'File Synchronization', 'C#', 'System Integration'],
       category: 'System Development'
     }
@@ -69,16 +69,16 @@ const ProjectsSection: React.FC = () => {
               <div className="project-media">
                 <iframe 
                   className="project-video"
-                  src={`https://www.youtube.com/embed/${project.youtubeId}?autoplay=1&loop=1&playlist=${project.youtubeId}&mute=1&controls=0&showinfo=0&rel=0&modestbranding=1&vq=hd1080&quality=hd1080&hd=1`}
+                  src={`https://player.vimeo.com/video/${project.vimeoId}?autoplay=1&loop=1&muted=1&controls=0&title=0&byline=0&portrait=0&quality=1080p&background=1`}
                   title={project.title}
                   frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allow="autoplay; fullscreen; picture-in-picture"
                   allowFullScreen
                 />
                 <button 
                   className="fullscreen-btn"
-                  onClick={() => openYouTubeFullscreen(project.youtubeId)}
-                  title="Assistir no YouTube"
+                  onClick={() => openVimeoFullscreen(project.vimeoId)}
+                  title="Assistir no Vimeo"
                 >
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
                     <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z" fill="currentColor"/>
